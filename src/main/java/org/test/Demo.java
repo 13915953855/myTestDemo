255a7ee1ad742b44132a6b0b67f9e4230d3b49ea
徐森 <xusen@cmiot.chinamobile.com>
2019-11-25 09:19:01 +0800

    123

--- src/main/java/org/test/Demo.java
+++ src/main/java/org/test/Demo.java
@@ -3,17 +3,22 @@ package org.test;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.util.DigestUtils;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
 
 public class Demo {
     public static void main(String[] args) {
-        String key = "KBm32ScoWpFoaTPBm70xSAHE";
-        String encode = encode3Des(key, "iotuser");
-        String encode1 = encode3Des(key, "iot123");
+        String key = "B20WVj7UEUkPDOYu2eghEQfP";
+        String encode = encode3Des(key, "CaHsE1u!");
+        String encode1 = encode3Des(key, "CF7hqp*Y");
         System.out.println("加密后的用户名：" + "DES@"+encode);
         System.out.println("加密后的密码：" + "DES@"+encode1);
+        String decode = resolveDESValue(key,encode);
+        System.out.println("解密："+decode);
     }
     private static byte[] hex(String key){
         String f = DigestUtils.md5DigestAsHex(key.getBytes());
@@ -49,6 +54,30 @@ public class Demo {
             e2.getMessage();
         }catch(Exception e3){
             e3.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String resolveDESValue(String key,String value) {
+        //将24位秘钥MD5加密
+        byte[] keyByte = hex(key);
+        //logger.info("key encoded by MD5:{}",keyByte);
+        //密文Base64解编码
+        byte[] src = Base64.decodeBase64(value.getBytes());
+        //logger.info("value decoded by Base64:{}",src);
+        try {
+            String deSeDe = "DESede";
+            //生成密钥
+            SecretKey deskey = new SecretKeySpec(keyByte, deSeDe);
+            //解密
+            Cipher c1 = Cipher.getInstance(deSeDe);
+            c1.init(Cipher.DECRYPT_MODE, deskey);
+            return new String(c1.doFinal(src));
+        } catch (java.security.NoSuchAlgorithmException e1) {
+        } catch (javax.crypto.NoSuchPaddingException e2){
+        } catch (BadPaddingException e3) {
+        } catch (IllegalBlockSizeException e4) {
+        } catch (InvalidKeyException e5) {
         }
         return null;
     }
--- src/main/java/org/test/Test.java
+++ src/main/java/org/test/Test.java
@@ -1,8 +1,8 @@
 package org.test;
 
+import org.test.linshi.Person;
 
-import org.test.linshi.MD5Utils;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
@@ -10,28 +10,9 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(MD5Utils.md5Bit32Lower("Hzyddl@2019"));
-        //1.求和
-       /* Integer sum = numbers.stream().reduce(Integer::sum).get();
-        System.out.println("总和:" + sum);
-        Integer min = numbers.stream().reduce(Integer::min).get();
-        System.out.println("最小数:" + min);
-        Integer max = numbers.stream().reduce(Integer::max).get();
-        System.out.println("最大数:" + max);
-
-        IntSummaryStatistics stats = numbers.stream().mapToInt(x -> x).summaryStatistics();
-        System.out.println("列表中最大的数 : " + stats.getMax());
-        System.out.println("列表中最小的数 : " + stats.getMin());
-        System.out.println("所有数之和 : " + stats.getSum());
-        System.out.println("平均数 : " + stats.getAverage());
-        //2.转成大写
-        List<String> strings = Arrays.asList("abc","hello");
-        List<String> tmp = strings.stream().map(String::toUpperCase).collect(Collectors.toList());
-        System.out.println(strings);
-        System.out.println(tmp);
-        //3.取出所有name等于A的id
-
-        //4.forEach
-        strings.stream().filter(x->!x.equals("xusen")).forEach(x->System.out.println(x));*/
+        String a = "${a}用电量${a}${a}度，进行告警";
+        a.replaceAll("\\$\\{a\\}","hello");
+        System.out.println(a);
     }
+
 }
